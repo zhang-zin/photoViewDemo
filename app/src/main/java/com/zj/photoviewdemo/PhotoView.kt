a10dff97cb9b7d7d6d3f9e1bb3fb199ac6d96130
zhangjin <798101251@qq.com>
2020-08-07 23:07:40 +0800

    1.添加手势操作

--- app/src/main/java/com/zj/photoviewdemo/PhotoView.kt
+++ app/src/main/java/com/zj/photoviewdemo/PhotoView.kt
@@ -7,7 +7,10 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.GestureDetector
 import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import android.view.View
+import android.widget.OverScroller
+import androidx.core.math.MathUtils
 
 class PhotoView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -17,10 +20,16 @@ class PhotoView @JvmOverloads constructor(
     private val bitmap = Utils.getBitmap(resources, IMAGE_WIDTH.toInt())
     private val paint = Paint()
     private val gestureDetector = GestureDetector(context, PhotoGestureDetector())
-    var scaleAnimator: ObjectAnimator = ObjectAnimator.ofFloat(this, "currentScale", 0f)
+    private val scaleGestureDetector = ScaleGestureDetector(
+        context, PhotoScaleGestureListener()
+    )
+    private val scaleAnimator: ObjectAnimator = ObjectAnimator.ofFloat(this, "currentScale", 0f)
+    val overScroller = OverScroller(context)
 
     private var originalOffsetX = 0F
     private var originalOffsetY = 0F
+    private var offSetX = 0F
+    private var offSetY = 0F
     private var smallScale = 0F
     private var bigScale = 0F
     private var currentScale = 0F
@@ -50,13 +59,26 @@ class PhotoView @JvmOverloads constructor(
 
         super.onDraw(canvas)
         canvas?.run {
+            // 双击缩小时，需要把图片放回中心位置,缩小时currentScale = smallScale，scaleFaction = 0
+            // 放大时currentScale = bigScale, scaleFaction = 1
+            val scaleFaction = (currentScale - smallScale) / (bigScale - smallScale)
+            translate(offSetX * scaleFaction, offSetY * scaleFaction)
             scale(currentScale, currentScale, width / 2F, height / 2F)
             drawBitmap(bitmap, originalOffsetX, originalOffsetY, paint)  // 把图片移到中间
         }
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        return gestureDetector.onTouchEvent(event)
+        var result = scaleGestureDetector.onTouchEvent(event)
+        if (!scaleGestureDetector.isInProgress) {
+            result = gestureDetector.onTouchEvent(event)
+        }
+        return result
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        scaleAnimator.cancel()
     }
 
     private fun getScaleAnimation(): ObjectAnimator {
@@ -64,6 +86,21 @@ class PhotoView @JvmOverloads constructor(
         return scaleAnimator
     }
 
+    /**
+     * 修正移动位置，不露出白边
+     */
+    private fun fixOffSet() {
+        offSetX = MathUtils.clamp(
+            offSetX,
+            -(bitmap.width * bigScale - width) / 2,
+            (bitmap.width * bigScale - width) / 2
+        )
+        offSetY = MathUtils.clamp(
+            offSetY,
+            -(bitmap.height * bigScale - height) / 2,
+            (bitmap.height * bigScale - height) / 2
+        )
+    }
 
     inner class PhotoGestureDetector : GestureDetector.SimpleOnGestureListener() {
         // up触发，单击或者双击的第一次会触发，up时如果不是双击的第二次点击，不是长按则触发
@@ -85,6 +122,17 @@ class PhotoView @JvmOverloads constructor(
             velocityX: Float,
             velocityY: Float
         ): Boolean {
+//            if (isEnlarge) {
+//                overScroller.fling(
+//                    offSetX.toInt(), offSetY.toInt(), velocityX.toInt(), velocityY.toInt(),
+//                    (-(bitmap.width * bigScale - width) / 2).toInt(),
+//                    ((bitmap.width * bigScale - width) / 2).toInt(),
+//                    (-(bitmap.height * bigScale - height) / 2).toInt(),
+//                    ((bitmap.height * bigScale - height) / 2).toInt(),
+//                    300, 300
+//                )
+//                postOnAnimation(FlingRunnable())
+//            }
             return super.onFling(e1, e2, velocityX, velocityY)
         }
 
@@ -92,6 +140,12 @@ class PhotoView @JvmOverloads constructor(
         override fun onDoubleTap(e: MotionEvent?): Boolean {
             isEnlarge = !isEnlarge
             if (isEnlarge) {
+                e?.run {
+                    // 放大是以图片的中心放大，为了更好的效果放大点需要移回手指点击的位置
+                    offSetX = (1 - bigScale / smallScale) * (x - width / 2)
+                    offSetY = (1 - bigScale / smallScale) * (y - height / 2)
+                    fixOffSet()
+                }
                 getScaleAnimation().start()
             } else {
                 getScaleAnimation().reverse()
@@ -112,6 +166,15 @@ class PhotoView @JvmOverloads constructor(
             distanceX: Float,
             distanceY: Float
         ): Boolean {
+            if (isEnlarge) {
+                offSetX -= distanceX
+                offSetY -= distanceY
+            } else {
+                offSetX += distanceX
+                offSetY += distanceY
+            }
+//            fixOffSet()
+            invalidate()
             return super.onScroll(e1, e2, distanceX, distanceY)
         }
 
@@ -137,6 +200,45 @@ class PhotoView @JvmOverloads constructor(
         override fun onLongPress(e: MotionEvent?) {
             super.onLongPress(e)
         }
+    }
+
+    inner class FlingRunnable : Runnable {
+        override fun run() {
+            if (overScroller.computeScrollOffset()) {
+                offSetX = overScroller.currX.toFloat()
+                offSetY = overScroller.currY.toFloat()
+                invalidate()
+                postOnAnimation(this)
+            }
+        }
+    }
+
+    inner class PhotoScaleGestureListener : ScaleGestureDetector.OnScaleGestureListener {
+
+        private var initScale = 0F;
+
+        override fun onScaleBegin(p0: ScaleGestureDetector?): Boolean {
+            initScale = currentScale
+            return true
+        }
+
+        override fun onScaleEnd(p0: ScaleGestureDetector?) {
+        }
+
+        override fun onScale(detector: ScaleGestureDetector?): Boolean {
+            detector?.run {
+                currentScale = initScale * scaleFactor
+                if (currentScale <= smallScale) {
+                    currentScale = smallScale
+                } else if (currentScale >= bigScale * 1.5F) {
+                    currentScale = bigScale * 1.5F
+                }
+                invalidate()
+            }
+//            isEnlarge = (currentScale >= smallScale)
+            return false
+        }
+
     }
 }
 
