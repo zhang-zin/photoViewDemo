35c7ec60e4fb33a6c8f0a501ef04b01a9317906b
zhangjin <15659515613@163.com>
2020-08-07 14:58:45 +0800

    1.第一次提交

---
+++ app/src/main/java/com/tongbu/sdk/photoviewdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tongbu.sdk.photoviewdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
